282b10c795dd424f14bb19c77e808712f5281490
Kiefer Wheeler <kieferw140@gmail.com>
2021-11-20 21:44:22 -0600

    commit

--- work.js
+++ work.js
@@ -1,19 +1,2 @@
-let button = document.getElementById('button1');
-let p = document.getElementById('hiddenp1');
-let img = document.getElementById('hiddenimg');
-let how = document.getElementById('how')
 
-// hiding the elements initially until the button is pressed.
-
-p.hidden = true;
-img.hidden = true;
-how.hidden = true;
-
-// when you click the button element it unhides the p and img
-
-button.onclick = function() {
-    p.hidden = false;
-    img.hidden = false;
-    how.hidden = false;
-};
 
