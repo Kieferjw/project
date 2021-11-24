eca516e3e21bda0a2e32b4ff8dc1b51e481926af
Kiefer Wheeler <kieferw140@gmail.com>
2021-11-24 09:07:27 -0600

    added java for .hidden and some other stuff

--- work.js
+++ work.js
@@ -1,2 +1,11 @@
+let button = document.getElementById('hiddenb');
+let p = document.getElementById('hiddenp')
+let img = document.getElementById('patrick')
 
+p.hidden = true;
+img.hidden = true;
 
+button.onclick = function () {
+    p.hidden = false;
+    img.hidden = false;
+}
